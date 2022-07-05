02e3a3e246cb6eaa20ee243d8e80c80da615d119
ikechiafrics <93938295+ikechiafrics@users.noreply.github.com>
2022-07-05 12:41:24 -0700

    Created  a functional Logout Button on the profile page

--- app/src/main/java/com/example/metachef/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/metachef/Fragments/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.metachef.Fragments;
 
+import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 
@@ -10,10 +11,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.example.metachef.LoginActivity;
 import com.example.metachef.R;
 import com.parse.ParseFile;
 import com.parse.ParseUser;
@@ -21,6 +24,7 @@ import com.parse.ParseUser;
 //This class represents the profile page
 public class ProfileFragment extends Fragment {
     private ImageView ivProfileImg;
+    private ImageView BtnLogout;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -40,10 +44,20 @@ public class ProfileFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        BtnLogout = view.findViewById(R.id.BtnLogout);
         ivProfileImg = view.findViewById(R.id.ivProfileImg);
         ParseUser user = ParseUser.getCurrentUser();
         ParseFile img = user.getParseFile("profile_picture");
         Glide.with(getContext()).load(img.getUrl()).transform(new RoundedCorners(200)).into(ivProfileImg);
         super.onViewCreated(view, savedInstanceState);
+        BtnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logOut();
+                ParseUser currentUser = ParseUser.getCurrentUser();
+                Intent i = new Intent(getContext(), LoginActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
